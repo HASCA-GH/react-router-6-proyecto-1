49dce39922f1ebb1dc3db092f454ff7799eec695
Humberto Asca <hasca@ciplima.org.pe>
2022-12-07 12:39:25 -0500

    Applying fake authentication

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useAuth } from './auth';
 
 const Menu = () => {
-  return (
+  const auth = useAuth()  
+
+    return (
     <nav>
       <ul>
-        {routes.map(route => (
+        {routes.map(route =>{
+          if (route.publicOnly && auth.user) return null
+          if (route.private && !auth.user) return null
+            return (
+              <li key={route.text}>
+              <NavLink 
+                style={({isActive})=>({
+                  color: isActive ? 'red': 'blue'})}
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+            )
+        })}
+        {/* {routes.map(route => (
           <li key={route.text}>
             <NavLink 
               style={({isActive})=>({
@@ -15,7 +33,7 @@ const Menu = () => {
               {route.text}
             </NavLink>
           </li>
-        ))}
+        ))} */}
         {/* <li>
           <Link to={"/"}>HomePage</Link>
         </li>
@@ -52,22 +70,28 @@ const routes = []
 routes.push({
   to:'/',
   text: 'Home',
+  private: false
 })
 routes.push({
   to:'/blog',
   text: 'Blog',
+  private: false
 })
 routes.push({
   to:'/profile',
   text: 'Profile',
+  private: true
 })
 routes.push({
   to:'/login',
   text: 'Login',
+  private: false,
+  publicOnly: true
 })
 routes.push({
   to:'/logout',
   text: 'Logout',
+  private: true
 })
 
 export {Menu}
